a2c2ae2f27e67c1afcaea333c0719e868427766b
musha <mushagh@gmail.com>
2018-12-03 09:44:38 +0800

    add min date

--- footer.php
+++ footer.php
@@ -4,6 +4,21 @@
   <script src="js/init.js"></script>
   <script>
     $(document).ready(function() {
+      var todaysDate = new Date();
+
+      function convertDate(date) {
+        var yyyy = date.getFullYear().toString();
+        var mm = (date.getMonth()+1).toString();
+        var dd  = date.getDate().toString();
+
+        var mmChars = mm.split('');
+        var ddChars = dd.split('');
+
+        return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+      }
+
+      var dateToday = convertDate(todaysDate);
+      // console.log(convertDate(todaysDate)); // Returns: 2015-08-25
       // Initialize modal
       $('.modal').modal();
 
@@ -12,7 +27,8 @@
 
       // Initialize datepicker
       $('.datepicker').pickadate({
-        format: 'yyyy-mm-dd'
+        format: 'yyyy-mm-dd',
+        min: dateToday
       });
 
       // Hide messagebox after 5 second
